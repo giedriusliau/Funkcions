b9281862a29185d3b4ee84978cf91aeb157ed2ba
giedriusliau <giedriusliaudenskas@gmail.com>
2024-03-19 16:32:44 +0200

    Functions

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
-
-
-
-
-
+        int a = 5;
+        int b = 10;
+        System.out.println("--- 1 ----");
+        suma(a, b);
+        System.out.println("--- 2 ----");
+        System.out.println( pisq() );
+        System.out.println("--- 3 ----");
 
     }
+
+    public static double pisq() {
+        return 9.8596;
+    }
+
+    public static void suma(int a, int b) {
+        int sum = a + b;
+        System.out.println("Suma: " + sum);
+
+    }
+
 }
